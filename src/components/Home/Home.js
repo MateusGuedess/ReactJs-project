a01a4d57ea83358345a7ff9813074560251c7073
Mateus Guedes <teuguedes@outlook.com>
2017-11-28 08:09:39 -0200

    Commit inicial

---
+++ src/components/Home/Home.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+class Home extends Component {
+    render(){
+        let heading = 'Soaring to new heights'
+        let subheading = 'Lorem ipsum dolor sit amet, consectetur adispciing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        return(
+            <div>
+                <section className='hero'>
+                    <div className='hero-body'>
+                        <div className='container'>
+                            <h1 className='title'>{ heading }</h1>
+                            <div className='is-two-thirds column is-paddingless'>
+                                <h2 className='subtitle is-4'>{ subheading }</h2>
+                            </div>
+                            <a className="button is-large is-primary" id='learn'>Learn More</a>
+                        </div>
+                    </div>
+                </section>
+
+                <section className='section'>
+                    <div className='container'>
+                        <div className='columns pd is-desktop'>
+                            <div className='column is-1 has-text-centered'>
+                                <i className='fa fa-cog is-primary'></i>
+                            </div>
+                            <div className='column is-one-third-desktop'>
+                                <p className='title'><strong> We provide superior logisticsso that yours business can succeed in a crazy word.</strong></p>
+                            </div>
+                            <div className='column'>
+                                <p>Lorem ipsum dolor sit amet, consectetur adispciing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <div className='columns pd'>
+                    <div className='column'>
+                        <div className='card'>
+                            <div className='card-content'>
+                                <p className='title'>"I think it's an absolutely excellent tool for our business"</p>
+                                <p className='subtitle'>- Gary Simon</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <div className='card-content'>
+                                <p className='title'>"I think it's an absolutely excellent tool for our business"</p>
+                                <p className='subtitle'>- Gary Simon</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <div className='card-content'>
+                                <p className='title'>"I think it's an absolutely excellent tool for our business"</p>
+                                <p className='subtitle'>- Gary Simon</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
